4bc3de3e27fdf6f8392f97a9c2d581707ac88d67
RepositorioGitH <menamartin722@gmail.com>
2023-10-13 17:49:18 -0600

    Primer Commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = 'a6b0e379675185cbc883f86716c645b1';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const min = document.querySelector('.weather-box .min');
+            const max = document.querySelector('.weather-box .max');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'assets/clear.png';
+                    break;
+
+                case 'Rain':
+                    image.src = 'assets/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'assets/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'assets/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'assets/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            max.innerHTML = `<span>Max: </span>${parseInt(json.main.temp_max)}<span>°C</span>`;
+            min.innerHTML = `<span>Min: </span>${parseInt(json.main.temp_min)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+
+        });
+
+
+});
